5b405c281de46bfd2df6699d7d79c5bf466eb47d
Ralph Maron Eda <edaralphmaron@gmail.com>
2024-10-11 21:53:12 +0800

    updated

--- js/dashboard.js
+++ js/dashboard.js
@@ -3,5 +3,5 @@ var backBtn = document.getElementById('btn-logout');
 backBtn.addEventListener('click', function () {
   // window.history.back();
 
-  window.location.href = 'home.html';
+  window.location.href = 'index.html';
 });
