1b70538301e0cb291f41142d8e05e0741fa56f35
Ralph Maron Eda <edaralphmaron@gmail.com>
2024-10-11 21:49:01 +0800

    updated

---
+++ js/auth.js
@@ -0,0 +1,17 @@
+var usernameInput = document.getElementById('username-input');
+var passwordInput = document.getElementById('password-input');
+var loginBtn = document.getElementById('login-btn');
+
+loginBtn.addEventListener('click', function () {
+  var username = usernameInput.value;
+  var passwd = passwordInput.value;
+
+  if (
+    (username === 'root' && passwd === 'toor') ||
+    (username === 'ralphmaron' && passwd === 'iscute')
+  ) {
+    window.location.href = 'dashboard.html';
+  } else {
+    alert('Incorrect password.');
+  }
+});
---
+++ js/dashboard.js
@@ -0,0 +1,7 @@
+var backBtn = document.getElementById('btn-logout');
+
+backBtn.addEventListener('click', function () {
+  // window.history.back();
+
+  window.location.href = 'home.html';
+});
